d03caf229f4f4c77a3ce537e5204201dfc34bcbf
andrea Díaz <dima0830@gmail.com>
2020-05-07 06:54:26 -0500

    muchos cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  
+
   var contador = 1;
   $('.menu_bar').click(function(){
     // $('nav').toggle();
@@ -31,11 +31,40 @@ $(".btnVerMas").click(function(){
 });
 
  $('.sliderlogo').slick({
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
   autoplaySpeed: 2500,
     arrows: true,
+    dots: false,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+      }
+    },
+     {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+      }
+    }
+  ]
+  });
+
+  $('.sliderlogo2').slick({
+    slidesToShow: 10,
+    slidesToScroll: 1,
+    autoplay: true,
+  autoplaySpeed: 2500,
+    arrows: false,
     dots: false,
     responsive: [
     {
@@ -70,71 +99,6 @@ $(".btnVerMas").click(function(){
         return false;
     });
 
-
-//
-
- function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-  }
-  // If element is scrolled into view, fade it in
-  $(window).scroll(function() {
-    $('.scroll-animations .animated').each(function() {
-      if (isScrolledIntoView(this) === true) {
-        $(this).addClass('fadeInLeft');
-      }
-    });
-  });
-
-  // Click Animations
-  $('button').on('click', function() {
-    /*
-    If any input is empty make it's border red and shake it. After the animation is complete, remove the shake and animated classes so that the animation can repeat.
-    */
-    
-    // Check name input
-    if ($('#name').val() === '') {
-      $('#name').addClass('form-error animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $(this).removeClass('animated shake');
-      });
-    } else {
-      $('#name').removeClass('form-error');
-    }
-    
-    // Check email input
-    if ($('#email').val() === '') {
-      $('#email').addClass('form-error animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $(this).removeClass('animated shake');
-      });
-    } else {
-      $('#email').removeClass('form-error');
-    }
-
-    // Check message textarea
-    if ($('#message').val() === '') {
-      $('#message').addClass('form-error animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $(this).removeClass('animated shake');
-      });
-    } else {
-      $('#message').removeClass('form-error');
-    }
-
-  });
-  
-  // Activate hinge effect when h4 is click in last section
-  $('.funky-animations h4').on('click', function() {
-    $(this).addClass('animated hinge').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-        $(this).removeClass('animated hinge');
-      });
-  });
-
-
-
 // scroll
 $('a[data-scroll]').click(function(e) {
   e.preventDefault();
@@ -149,5 +113,81 @@ $('a[data-scroll]').click(function(e) {
 }); 
 
 
+});
 
+$(document).ready(function() {
+  
+  var slideStart;
+
+  function startSlide() {
+    slideStart = setInterval(slideShow, 10000);
+  };
+  function slideShow() {
+
+    var slideCurrent = $(".bn-active");
+    var slideNext = slideCurrent.next();
+    
+
+    if (slideNext.length == 0) {
+      slideNext = $(".bn").first();
+    }
+
+    var slideIndex = slideNext.index();
+
+    $('.bn').css({
+      'transform': 'translate(-' + (slideIndex) * 100 + '%)'
+    });
+
+    $('.bn').removeClass('bn-active');
+    slideNext.addClass('bn-active');
+
+    var captionNext = slideNext.find(".caption");
+    
+    
+
+  };
+  function parallaxX() {
+    var scrollTop = window.pageYOffset
+
+    $(window).on("scroll resize", function() {
+      scrollTop = window.pageYOffset;
+    });
+
+    $(".bn").each(function() {
+      var parallaxImage = $(this);
+      var parallaxOffset = parallaxImage.offset().top;
+      var yPos;
+      var coords;
+
+      $(window).on("scroll resize", function() {
+        yPos = -(parallaxOffset - scrollTop) / 2;
+        coords = '50% ' + yPos + 'px';
+
+        parallaxImage.css({
+          backgroundPosition: coords
+        });
+      });
+    });
+  };
+
+
+  $('.bn-indicator li').on("click", function() {
+
+    clearInterval(slideStart);
+    var goToSlide = $(this).index();
+
+    $('.bn-indicator li').removeClass('active');
+    $('.bn').removeClass('bn-active');
+    $('.bn').eq(goToSlide).addClass('bn-active');
+    $(this).addClass('active');
+
+    $('.bn').css({
+      'transform': 'translate(-' + (goToSlide) * 100 + '%)'
+    });
+    startSlide();
+  });
+
+  startSlide();
+  parallaxX();
+  siteNav();
 });
